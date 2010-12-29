b01134ffe216c596a63560fa415b0cadc48bf527
Eoin Bailey <baileye@gmail.com>
2010-12-29 17:02:58 +0000

    Working to load a map. Nothing else happens.

--- IrishSafetyCameras/src/org/dgtug/irishsafetycameras/Main.java
+++ IrishSafetyCameras/src/org/dgtug/irishsafetycameras/Main.java
@@ -1,13 +1,21 @@
 package org.dgtug.irishsafetycameras;
-
-import android.app.Activity;
+ 
+//import android.app.Activity;
+import com.google.android.maps.MapActivity;
+//import com.google.android.maps.MapView;
 import android.os.Bundle;
 
-public class Main extends Activity {
+public class Main extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    /** Called when the activity is started (i.e. loaded by the user) */
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
